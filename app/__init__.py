48a6fcd6456ef44f7513c4e0561457cc4a157e4c
Alberto Demian López Vázquez <alberto.lopez8744@alumnos.udg.mx>
2023-04-13 13:18:14 -0600

    update final

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,259 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+from flask import render_template, request, redirect, session
+from flaskext.mysql import MySQL
+import psycopg2
+from datetime import datetime
+from flask import send_from_directory
 
 app = Flask(__name__)
+app.secret_key="develoteca"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+
+def conectar_db():
+    conexion = psycopg2.connect(
+        user = 'postgres',
+        password = 'password',
+        host = 'localhost',
+        port = '5432',
+        database = 'UDGDB'
+    )
+    return conexion
+
+
+#Sitio principal
+@app.route('/')
+def inicio():
+    return render_template('sitio/index.html')
+
+@app.route('/img/<imagen>')
+def imagenes(imagen):
+    print(imagen)
+    return send_from_directory(os.path.join('templates/sitio/img'),imagen)
+
+@app.route('/css/<archivocss>')
+def css_link(archivocss):
+    return send_from_directory(os.path.join("templates/sitio/css"),archivocss)
+
+
+# Tablas
+@app.route('/tablas')
+def tables():
+    return render_template('sitio/tablas.html')
+
+@app.route('/tablas/<tabla>')
+def tablas_seleccion(tabla):
+    print(tabla)
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    if(tabla == 'administrativo'):
+        query = 'SELECT * FROM administrativo ORDER BY id'
+    if (tabla == 'centro'):
+        query = 'SELECT id_cent,nombre_cent,localizacion_cent,nombre FROM centro, administrativo WHERE id_adminis = id ORDER BY id_cent'
+    if(tabla == 'campus'):
+        query = 'SELECT id_camp,nombre_camp,localizacion_camp,nombre,nombre_cent FROM campus, centro, administrativo WHERE campus.id_adminis = administrativo.id and campus.id_cent = centro.id_cent ORDER BY id_camp'
+    if(tabla == 'carrera'):
+        query = 'SELECT id_car,nombre_car,cupos_car,nombre,nombre_camp FROM carrera, campus, administrativo WHERE carrera.id_adminis = administrativo.id and carrera.id_camp = campus.id_camp ORDER BY id_car'
+    cursor.execute(query)
+    seleccion = cursor.fetchall()
+    conexion.commit()
+    return render_template('sitio/tablas.html',tabla=tabla, seleccions=seleccion)
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('sitio/nosotros.html')
+
+
+# Caracteristicas de Administrador
+@app.route('/admin/')
+def admin_index():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    return render_template('admin/index.html')
+
+@app.route('/admin/login')
+def admin_login():
+    if "login" in session:
+        return redirect("/admin")
+    
+    return render_template('admin/login.html')
+
+@app.route('/admin/login', methods=['POST'])
+def admin_login_post():
+    
+    _usuario = request.form['txtUsuario']
+    _password = request.form['txtPassword']
+    print(_usuario)
+    print(_password)
+
+    if _usuario == "admin" and _password == "123":
+        session["login"] = True
+        session["usuario"] = "Administrador"
+        return redirect("/admin")
+    
+    return render_template("admin/login.html", mensaje = "Acceso denegado")
+
+@app.route('/admin/cerrar')
+def admin_cerrar():
+    session.clear()
+    return redirect('/admin/login')
+
+ # Administracion de datos
+@app.route('/admin/administrativo')
+def admin_administrativo():
+
+    if not 'login' in session:
+        return redirect('/admin/login')
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM administrativo ORDER BY id")
+    administrativo = cursor.fetchall()
+    conexion.commit()
+    print(administrativo)
+
+    return render_template('admin/administrativo.html', administrativo=administrativo)
+
+@app.route('/admin/administrativo/guardar', methods=['POST'])
+def admin_administrativo_guardar():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _nombre = request.form['txtNombre']
+   
+    sql = "INSERT INTO administrativo (id, nombre) VALUES (DEFAULT, '"+_nombre+"');"
+    
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute(sql)
+    conexion.commit()
+
+    print(_nombre)
+    return redirect('/admin/administrativo')
+
+@app.route('/admin/administrativo/borrar', methods=['POST'])
+def admin_administrativo_borrar():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _id = request.form['txtID']
+    print(_id)
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM administrativo WHERE id=%s",(_id))
+    conexion.commit()
+    return redirect("/admin/administrativo")
+
+    
+#PArte de administracion de datos
+
+@app.route('/admin/autos')
+def admin_autos():
+
+    if not 'login' in session:
+        return redirect('/admin/login')
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM auto ORDER BY id")
+    autos = cursor.fetchall()
+    conexion.commit()
+    print(autos)
+
+    return render_template('admin/autos.html', autos=autos)
+
+@app.route('/admin/autos/guardar', methods=['POST'])
+def admin_autos_guardar():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _matricula = request.form['txtMatricula']
+    _marca = request.form['txtMarca']
+    _modelo = request.form['txtModelo']
+
+    sql = "INSERT INTO auto (id, matricula, marca, modelo) VALUES (DEFAULT, %s, %s, %s);"
+    datos=(_matricula,_marca,_modelo)
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute(sql,datos)
+    conexion.commit()
+
+    print(_matricula)
+    print(_marca)
+    print(_modelo)
+
+    return redirect('/admin/autos')
+
+@app.route('/admin/autos/borrar', methods=['POST'])
+def admin_autos_borrar():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _id = request.form['txtID']
+    print(_id)
+
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM auto WHERE id=%s",(_id))
+    conexion.commit()
+
+
+    return redirect("/admin/autos")
+@app.route('/admin/autos/edit', methods=['POST'])
+def admin_edit_auto():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _id = request.form['txtID']
+    print(_id)
+    
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    sql = "SELECT * FROM auto WHERE id=%s"
+    cursor.execute(sql,_id)
+    autos = cursor.fetchall()
+    conexion.commit()
+    print(autos)
+
+    return render_template('admin/update.html', autos=autos)
+
+@app.route('/admin/autos/update', methods={'POST'})
+def admin_auto_update():
+    if not 'login' in session:
+        return redirect('/admin/login')
+    _id = request.form['txtID']
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    sql = "SELECT * FROM auto WHERE id=%s"
+    cursor.execute(sql,_id)
+    autoOld = cursor.fetchall()
+    conexion.commit()
+    print(autoOld)
+
+    _matricula = request.form['txtMatricula']
+    if _matricula == '':
+        print("Si esta vacia matricula")
+        _matricula = autoOld[0][1]
+    _marca = request.form['txtMarca']
+    if _marca == '':
+        print("Si esta vacia marca")
+        _marca = autoOld[0][2]
+    _modelo = request.form['txtModelo']
+    if _modelo == '':
+        print("Si esta vacia modelo")
+        _modelo = autoOld[0][3]
+    print(_id)
+    print(_matricula)
+    print(_marca)
+    print(_modelo)
+
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    sql = "UPDATE auto SET matricula='"+_matricula+"', marca='"+_marca+"', modelo='"+_modelo+"' WHERE id="+_id
+    cursor.execute(sql)
+    conexion.commit()
+
+
+    return redirect("/admin/autos")
+
+if __name__ == '__main__':
+    app.run(debug=True)
